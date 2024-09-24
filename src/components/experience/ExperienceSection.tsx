f94a9011f8902c8e625a7e665f673af309d8e14a
Noam Moskowitz <noammz101@gmail.com>
2024-09-24 16:36:40 +0300

    padding fixes

--- src/components/experience/ExperienceSection.tsx
+++ src/components/experience/ExperienceSection.tsx
@@ -7,9 +7,9 @@ interface ExperienceSectionProps {
 
 const ExperienceSection: React.FC<ExperienceSectionProps> = ({ title, content }) => {
   return (
-    <div className="flex flex-col pl-12 border-l-2 border-divider">
+    <div className="flex flex-col pl-5 md:pl-12 border-l-2 border-divider">
       <h3 className="font-bold">{title}</h3>
-      <p className="pl-10 pt-4">{content}</p>
+      <p className="md:pl-10 pt-4">{content}</p>
     </div>
   );
 };
